acaea68e0ee392a53fc24f0eed3d18d3b21507c3
Lester Caine <lester@lsces.co.uk>
2005-08-06 10:04:41 +0000

    PAckage multiple updates in a single transaction

--- edit_structure_inc.php
+++ edit_structure_inc.php
@@ -3,7 +3,7 @@
  * edit_structure_inc
  *
  * @author   Christian Fowler>
- * @version  $Revision: 1.1.1.1.2.6 $
+ * @version  $Revision: 1.1.1.1.2.7 $
  * @package  liberty
  * @subpackage functions
  */
@@ -23,6 +23,7 @@ if( empty( $_REQUEST["structure_id"] ) ) {
 } else {
 	global $gStructure;
 	$gStructure = new LibertyStructure( $_REQUEST["structure_id"] );
+	$gStructure->mDb->StartTrans();
 	$gStructure->load();
 	
 	// order matters for these conditionals
@@ -125,7 +126,7 @@ if( empty( $_REQUEST["structure_id"] ) ) {
 
 	$gBitSmarty->assign( (!empty( $_REQUEST['tab'] ) ? $_REQUEST['tab'] : 'body').'TabSelect', 'tdefault' );
 	$gBitSmarty->assign('subtree', $rootTree = $rootStructure->getSubTree( $rootStructure->mStructureId ));
-
+	$gStructure->mDb->CompleteTrans();
 }
 
 ?>
