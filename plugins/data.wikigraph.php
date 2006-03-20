d7dabc69deed56e72654e10ed3457d14feb53942
bitweaver.org <bitweaver@users.sourceforge.net>
2006-03-20 09:19:56 +0000

    get rid of non-object warning

--- plugins/data.wikigraph.php
+++ plugins/data.wikigraph.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Revision: 1.7 $
+ * @version  $Revision: 1.8 $
  * @package  liberty
  * @subpackage plugins_data
  */
@@ -18,7 +18,7 @@
 // | by: wolff_borg <wolff_borg@yahoo.com.au>
 // | Reworked from: wikiplugin_wikigraph.php - see deprecated code below
 // +----------------------------------------------------------------------+
-// $Id: data.wikigraph.php,v 1.7 2006/02/01 17:02:16 bitweaver Exp $
+// $Id: data.wikigraph.php,v 1.8 2006/03/20 09:19:56 bitweaver Exp $
 /**
  * definitions
  */
@@ -125,40 +125,42 @@ function data_wikigraph($data, $params) {
 	if(isset($nodeheight)) $add.="&amp;nodeheight=$nodeheight";
 	if(isset($edgecolor)) $add.="&amp;edgecolor=$edgecolor";
 	if(isset($edgestyle)) $add.="&amp;edgestyle=$edgestyle";
-	if(empty($data)) $data=$gContent->mInfo['title'];
-	$mapname=md5(uniqid("."));
-	$ret='';
-	$garg = array(
-		'att' => array(
-			'level'     => $level,
-			'nodesep'   => isset($nodesep) ? $nodesep : ".1",
-			'rankdir'   => isset($rankdir) ? $rankdir : "LR",
-			'bgcolor'   => isset($bgcolor) ? $bgcolor : "transparent",
-			'size'      => isset($size) ? $size : ""
-		),
-		'node' => array(
-			'fontsize'  => isset($fontsize) ? $fontsize : "9",
-			'fontname'  => isset($fontname) ? $fontname : "Helvetica",
-			'shape'     => isset($shape) ? $shape : "box",
-			'style'     => isset($nodestyle) ? $nodestyle : "filled",
-			'color'     => isset($nodecolor) ? $nodecolor : "#aeaeae",
-			'fillcolor' => isset($nodefillcolor) ? $nodefillcolor : "#FFFFFF",
-			'width'     => isset($nodewidth) ? $nodewidth : ".1",
-			'height'    => isset($nodeheight) ? $nodeheight : ".1"
-		),
-		'edge' => array(
-			'color'     => isset($edgecolor) ? $edgecolor : "#999999",
-			'style'     => isset($edgestyle) ? $edgestyle : "solid"
-		)
-	);
-	$ret .= "<div align='center'><img border='0' src=\"".WIKI_PKG_URL."wiki_graph.php?page=".urlencode($data)."&amp;level=$level$add\" alt='graph' usemap='#$mapname' />";
-	if( !empty( $page ) && !empty( $level ) && !empty( $garg ) ) {
-		$mapdata = $wikilib->get_graph_map($page, $level, $garg);
-		$mapdata = preg_replace("/\n|\r/", '', $mapdata);
-		$ret .= "<map name='$mapname'>$mapdata</map></div>";
-		return $ret;
-	} else {
-		return ' ';
+	if(empty($data)) $data=( empty( $gContent->mInfo['title'] ) ? NULL : $gContent->mInfo['title'] );
+	if( !empty( $data ) ) {
+		$mapname=md5(uniqid("."));
+		$ret='';
+		$garg = array(
+			'att' => array(
+				'level'     => $level,
+				'nodesep'   => isset($nodesep) ? $nodesep : ".1",
+				'rankdir'   => isset($rankdir) ? $rankdir : "LR",
+				'bgcolor'   => isset($bgcolor) ? $bgcolor : "transparent",
+				'size'      => isset($size) ? $size : ""
+			),
+			'node' => array(
+				'fontsize'  => isset($fontsize) ? $fontsize : "9",
+				'fontname'  => isset($fontname) ? $fontname : "Helvetica",
+				'shape'     => isset($shape) ? $shape : "box",
+				'style'     => isset($nodestyle) ? $nodestyle : "filled",
+				'color'     => isset($nodecolor) ? $nodecolor : "#aeaeae",
+				'fillcolor' => isset($nodefillcolor) ? $nodefillcolor : "#FFFFFF",
+				'width'     => isset($nodewidth) ? $nodewidth : ".1",
+				'height'    => isset($nodeheight) ? $nodeheight : ".1"
+			),
+			'edge' => array(
+				'color'     => isset($edgecolor) ? $edgecolor : "#999999",
+				'style'     => isset($edgestyle) ? $edgestyle : "solid"
+			)
+		);
+		$ret .= "<div align='center'><img border='0' src=\"".WIKI_PKG_URL."wiki_graph.php?page=".urlencode($data)."&amp;level=$level$add\" alt='graph' usemap='#$mapname' />";
+		if( !empty( $page ) && !empty( $level ) && !empty( $garg ) ) {
+			$mapdata = $wikilib->get_graph_map($page, $level, $garg);
+			$mapdata = preg_replace("/\n|\r/", '', $mapdata);
+			$ret .= "<map name='$mapname'>$mapdata</map></div>";
+			return $ret;
+		} else {
+			return ' ';
+		}
 	}
 }
 }
