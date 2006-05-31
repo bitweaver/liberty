107aa99182da137ed225eeb09e2a7e23373c55bc
Lester Caine <lester@lsces.co.uk>
2006-05-31 23:08:51 +0000

    Restore basic paginate - not sure where this is going, but simple list should work anyway

--- get_content_list_inc.php
+++ get_content_list_inc.php
@@ -3,7 +3,7 @@
  * get_content_list
  *
  * @author   Christian Fowler>
- * @version  $Revision: 1.11 $
+ * @version  $Revision: 1.12 $
  * @package  liberty
  * @subpackage functions
  */
@@ -23,10 +23,11 @@ if( empty( $gContent ) || !is_object( $gContent ) ) {
 if( empty( $contentListHash ) ) {
 	$contentListHash = array(
 		'content_type_guid' =>   $contentSelect = empty( $_REQUEST['content_type_guid'] ) ? NULL : $_REQUEST['content_type_guid'],
-		'offset' =>              !empty( $offset_content ) ? $offset_content : 0,
+		'offset' =>              !empty( $offset_content ) ? $offset_content : NULL,
 		'max_records' =>         !empty( $max_content ) ? $max_content : 100,
 		'sort_mode' =>           !empty( $content_sort_mode ) ? $content_sort_mode : 'title_asc',
 		'find' =>                !empty( $_REQUEST["find_objects"] ) ? $_REQUEST["find_objects"] : NULL,
+		'page' =>                !empty( $_REQUEST["list_page"] ) ? $_REQUEST["list_page"] : NULL,
 		'user_id' =>             @BitBase::verifyId( $_REQUEST['user_id'] ) ? $_REQUEST['user_id'] : NULL,
 	);
 }
