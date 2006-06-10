dfb8fef8052a0024737432b293c9229d4aefe309
Max Kremmel <xing@synapse.plus.com>
2006-06-10 16:18:42 +0000

    move uploadMax calculation to seperate file for easy inclusion

---
+++ calculate_max_upload_inc.php
@@ -0,0 +1,17 @@
+<?php
+// settings that are useful to know about at upload time
+$postMax = str_replace( 'M', '', ini_get( 'post_max_size' ));
+$uploadMax = str_replace( 'M', '', ini_get( 'upload_max_filesize' ) );
+
+if( $postMax < $uploadMax ) {
+	$uploadMax = $postMax;
+}
+
+// calculate user quota
+if( $gBitSystem->isPackageActive( 'quota' ) ) {
+	require_once( QUOTA_PKG_PATH.'calculate_quota_inc.php' );
+}
+
+$gBitSmarty->assign( 'uploadMax', $uploadMax );
+?>
+
