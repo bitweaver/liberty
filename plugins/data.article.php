1ad9e3f1cb2aca9c942e0f54d36cc0c4dd2d1c13
Stephan Borg <wolff_borg@users.sourceforge.net>
2006-06-11 02:08:33 +0000

    Fixed article plugin and added truncated format option

--- plugins/data.article.php
+++ plugins/data.article.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Revision: 1.1.1.1.2.8 $
+ * @version  $Revision: 1.1.1.1.2.9 $
  * @package  liberty
  * @subpackage plugins_data
  */
@@ -18,12 +18,12 @@
 // | by: StarRider <starrrider@users.sourceforge.net>
 // | Reworked from: wikiplugin_article.php - see deprecated code below
 // +----------------------------------------------------------------------+
-// $Id: data.article.php,v 1.1.1.1.2.8 2005/11/11 22:04:08 mej Exp $
+// $Id: data.article.php,v 1.1.1.1.2.9 2006/06/11 02:08:33 wolff_borg Exp $
 
 /**
  * definitions
  */
-global $gBitSystem;
+global $gBitSystem, $gBitSmarty;
 if( $gBitSystem->isPackageActive( 'articles' ) ) { // Do not include this Plugin if the Package is not active
 define( 'PLUGIN_GUID_DATAARTICLE', 'dataarticle' );
 global $gLibertySystem;
@@ -31,14 +31,13 @@ $pluginParams = array ( 'tag' => 'ARTICLE',
 						'auto_activate' => FALSE,
 						'requires_pair' => FALSE,
 						'load_function' => 'data_article',
-						'title' => 'Article<strong> - This plugin is not yet functional.</strong>', // Remove this line when the plugin
-//						'title' => 'Article',                                                                             // and Remove the comment from the start of this line
+						'help_function' => 'data_article_help',
+						'title' => 'Article',
 						'help_page' => 'DataPluginArticle',
 						'description' => tra("This plugin will display the data from a single field in the specified Article."),
-						'help_function' => 'data_article_help',
-						'syntax' => "{ARTICLE id= field=}",
+						'syntax' => "{ARTICLE id= field= format=}",
 						'plugin_type' => DATA_PLUGIN
-					  );
+					);
 $gLibertySystem->registerPlugin( PLUGIN_GUID_DATAARTICLE, $pluginParams );
 $gLibertySystem->registerDataTag( $pluginParams['tag'], PLUGIN_GUID_DATAARTICLE );
 
@@ -62,6 +61,11 @@ function data_article_help() {
 				.'<td>' . tra( "This can be any field found in the") . ' tiki_articles ' . tra("table.") . '<br />'
 				. tra("This includes (in order of usefulnes):") . '<strong>title, heading, body, author, size, reads, votes, points, type and rating</strong>' . '</td>'
 			.'</tr>'
+			.'<tr class="odd">'
+				.'<td>format</td>'
+				.'<td>' . tra( "string") . '<br />' . tra("(optional)") . '</td>'
+				.'<td>' . tra( "Specify format for article display - options: full, list (default)") . '</td>'
+			.'</tr>'
 		.'</table>'
 		. tra("Example: ") . "{ARTICLE id=14 field='heading'}";
 	return $help;
@@ -69,32 +73,42 @@ function data_article_help() {
 
 // Executable Routine
 function data_article($data, $params) { // No change in the parameters with Clyde
-// The next 2 lines allow access to the $pluginParams given above and may be removed when no longer needed
-	global $gLibertySystem; 
+	// The next 2 lines allow access to the $pluginParams given above and may be removed when no longer needed
+	global $gLibertySystem, $gBitSmarty;
 	$pluginParams = $gLibertySystem->mPlugins[PLUGIN_GUID_DATAARTICLE];
-	$ret = 'The plugin <strong>"' . $pluginParams['tag'] . '"</strong> has not been completed as yet. ';
-	return $ret;
-}
-}
-/******************************************************************************
-The code below is from the deprecated ARTICLE plugin. All comments and the help routines have been removed. - StarRider
 
-global $gBitSystem;
-if( $gBitSystem->isPackageActive( 'articles' ) ) {
-	include_once( ARTICLES_PKG_PATH . 'art_lib.php' );
+	require_once( ARTICLES_PKG_PATH.'BitArticle.php');
+	require_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
 
-	function wikiplugin_article($data, $params) {
-		global $artlib;
-		extract ($params, EXTR_SKIP);
-		if (!isset($id)) {
-			return ("<b>The plugin Article needs an article ID to function.</b><br/>");
+	$module_params = $params;
+	$display_result = "<strong>Article ID not found!</strong>";
+
+	$display_format = empty($module_params['format']) ? 'full' : $module_params['format'];
+
+	if( !empty( $module_params['id'] ) ) {
+		$articlesObject = new BitArticle($module_params['id']);
+		$articlesObject->load();
+		$article = $articlesObject->mInfo;
+
+		if ($display_format == "list") {
+			$gBitSmarty->assign( 'showDescriptionsOnly', TRUE );
+			$getHash['article_id'] = $module_params['id'];
+			$articles_results = $articlesObject->getList( $getHash );
+
+			$display_result = '<div class="articles">';
+			$gBitSmarty->assign( 'showDescriptionsOnly', TRUE );
+			foreach( $articles_results['data'] as $article ) {
+				$gBitSmarty->assign( 'article', $article );
+				$display_result .= $gBitSmarty->fetch( 'bitpackage:articles/article_display.tpl' );
+			}
+			$display_result .= '</div>';
+			$display_result = eregi_replace( "\n", "", $display_result );
+		} else {
+			$gBitSmarty->assign( 'article', $article );
+			$display_result = $gBitSmarty->fetch( 'bitpackage:articles/article_display.tpl' );
 		}
-		if (!isset($field)) {
-			$field = 'heading';
-		}
-		$article_data = $artlib->get_article($id);
-		return $article_data[$field];
 	}
+	return $display_result;
 }
-*/
+}
 ?>
