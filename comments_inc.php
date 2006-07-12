00092d752adc0141155d0fef5e47b0d3d0da03b1
Sylvie Greverend <sylvieg@users.sourceforge.net>
2006-07-12 16:47:15 +0000

    need the list of format_guid to know the format_guid value if only one if available

--- comments_inc.php
+++ comments_inc.php
@@ -3,12 +3,12 @@
  * comment_inc
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.17 $
+ * @version  $Revision: 1.18 $
  * @package  liberty
  * @subpackage functions
  */
 
-// $Header: /cvsroot/bitweaver/_bit_liberty/comments_inc.php,v 1.17 2006/07/11 21:17:20 sylvieg Exp $
+// $Header: /cvsroot/bitweaver/_bit_liberty/comments_inc.php,v 1.18 2006/07/12 16:47:15 sylvieg Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -82,7 +82,7 @@ if (!empty($_REQUEST['post_comment_submit']) && $gBitUser->hasPermission( 'p_lib
 	$storeRow['parent_id'] = (@BitBase::verifyId($storeComment->mInfo['parent_id']) ? $storeComment->mInfo['parent_id'] : (!@BitBase::verifyId($_REQUEST['post_comment_reply_id']) ? $commentsParentId : $_REQUEST['post_comment_reply_id']));
 	$storeRow['content_id'] = (@BitBase::verifyId($storeComment->mContentId) ? $storeComment->mContentId : NULL);
 	if (!empty( $_REQUEST['format_guid'] ) ) {
-	  $storeRow['format_guid'] = $_REQUEST['format_guid'];
+		$storeRow['format_guid'] = $_REQUEST['format_guid'];
 	}
 	$storeComment->storeComment($storeRow);
 }
@@ -105,6 +105,10 @@ if( !empty( $_REQUEST['post_comment_preview'] ) ) {
 	$postComment['format_guid'] = PLUGIN_GUID_TIKIWIKI;
 	$postComment['parsed_data'] = LibertyComment::parseData( $postComment );
 	$gBitSmarty->assign('post_comment_preview', TRUE);
+}
+
+if( !empty( $_REQUEST['post_comment_preview'] ) || $post_comment_request ) {
+	include_once( LIBERTY_PKG_PATH.'edit_help_inc.php'); // to set up the format_guid list
 }
 
 // $post_comment_reply_id is the content_id which a post is replying to
