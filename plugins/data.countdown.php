503d1a854690d002e1d8bd9047364da9cf808171
laetzer <laetzer@users.sourceforge.net>
2007-04-30 04:52:43 +0000

    double display bug fixed, new options punctuation + class, useful markup

--- plugins/data.countdown.php
+++ plugins/data.countdown.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Revision: 1.6 $
+ * @version  $Revision: 1.7 $
  * @package  liberty
  * @subpackage plugins_data
  */
@@ -17,7 +17,7 @@
 // | Reworked for Bitweaver (& Undoubtedly Screwed-Up)
 // | by: StarRider <starrrider@users.sourceforge.net>
 // +----------------------------------------------------------------------+
-// $Id: data.countdown.php,v 1.6 2006/04/06 05:06:11 starrrider Exp $
+// $Id: data.countdown.php,v 1.7 2007/04/30 04:52:43 laetzer Exp $
 
 /**
  * definitions
@@ -27,13 +27,13 @@ global $gLibertySystem;
 $pluginParams = array (
 	'tag' => 'COUNTDOWN',
 	'auto_activate' => TRUE,
-	'requires_pair' => TRUE,
+	'requires_pair' => FALSE,
 	'load_function' => 'data_countdown',
 	'title' => 'CountDown',
 	'help_page' => 'DataPluginCountDown',
 	'description' => tra("Displays a Count-Down until a date:time is reached - then - negative numbers indicate how long it has been since that date. The Count-Down is displayed in the format of (X days, X hours, X minutes and X seconds)."),
 	'help_function' => 'data_countdown_help',
-	'syntax' => "{COUNTDOWN enddate= localtime= }" . tra("Text") . "{countdown}",
+	'syntax' => "{COUNTDOWN enddate= localtime= class= punct= text=}",
 	'path' => LIBERTY_PKG_PATH.'plugins/data.countdown.php',
 	'security' => 'registered',
 	'plugin_type' => DATA_PLUGIN
@@ -52,7 +52,7 @@ function data_countdown_help() {
 			.'</tr>'
 			.'<tr class="odd">'
 				.'<td>enddate</td>'
-				.'<td>' . tra( "string") . '<br />' . tra("(Manditory)") . '</td>'
+				.'<td>' . tra( "string") . '<br />' . tra("(mandatory)") . '</td>'
 				.'<td>' . tra( "A date used to compare to the present date. Several date formats are accepted, but spelling it out like this: <strong>May 10 2004</strong> is probably the simplest. A time can be include with the date like this: <strong>20:02:00 or 8:02pm</strong> . There is <strong>NO</strong> Default.") . '</td>'
 			.'</tr>'
 			.'<tr class="even">'
@@ -60,33 +60,67 @@ function data_countdown_help() {
 				.'<td>' . tra( "boolean") . '<br />' . tra("(optional)") . '</td>'
 				.'<td>' . tra( "Determins if Local Time is displayed or not. Passing any value in this parameter will make it <strong>TRUE</strong>. The Default = <strong>FALSE</strong> so Local Time will not be displayed") . '</td>'
 			.'</tr>'
+			.'<tr class="odd">'
+				.'<td>class</td>'
+				.'<td>' . tra( "string") . '<br />' . tra("(optional)") . '</td>'
+				.'<td>' . tra( "Classname of the SPAN surrounding the countdown. The date/time segments are each wrapped in a VAR-Tag. Default = countdown") . '</td>'
+			.'</tr>'			
+			.'<tr class="even">'
+				.'<td>punct</td>'
+				.'<td>' . tra( "string") . '<br />' . tra("(optional)") . '</td>'
+				.'<td>' . tra( "Any kind of punctuation to divide the date/time segments from each other, a comma, a colon, a pipe ... Default = space. To put a non breaking space, use HTML: &amp;nbsp;") . '</td>'
+			.'</tr>'			
+			.'<tr class="odd">'
+				.'<td>text</td>'
+				.'<td>' . tra( "string") . '<br />' . tra("(optional)") . '</td>'
+				.'<td>' . tra( "Text to be displayed after the date/time string. It's wrapped in &lt;em&gt;.") . '</td>'
+			.'</tr>'			
 		.'</table>'
-		. tra("Example: ") . "{COUNTDOWN enddate='8:02pm May 10 2004' localtime='on'}" . tra(" - Time Passes So Slowly") . "{countdown}<br />"
-		. tra("Displays: <strong>82 days, 23 hours, 37 minutes and 31 seconds - Time Passes So Slowly</strong>");
+		.'<p>' . tra("Example 1: ") . '<input value="{COUNTDOWN enddate=\'8:02pm May 10 2004\' localtime=\'on\' text=\'' . tra(" - Time Passes So Slowly") . '\'}" type="text" size="40" /></p>'
+		.'<p>' . tra("Example 2: ") . '<input value="{COUNTDOWN enddate=\'2012-12-22 00:01\' class=\'alert red\' punct=\', \' text=\'Purple Haze\'}" type="text" size="40" /></p>'
+	;
 	return $help;
 }
 
 // Load Function
-function data_countdown($data, $params) {
-// The next 2 lines allow access to the $pluginParams given above
+function data_countdown($data,$params) {
+	// The next 2 lines allow access to the $pluginParams given above
 	global $gLibertySystem;
 	$pluginParams = $gLibertySystem->mPlugins[PLUGIN_GUID_DATACOUNTDOWN];
 	extract ($params, EXTR_SKIP);
-    if (!isset($enddate) ) {  // The Manditory Parameter is missing
-        $ret = tra("The required parameter ") . "<strong>enddate</strong>" . tra(" was missing from the plugin ") . '<strong>"' . $pluginParams['tag'] . '"</strong>';
+	
+	if (!isset($enddate) ) {  // The Mandatory Parameter is missing
+		$ret = tra("The required parameter ") . "<strong>enddate</strong>" . tra(" was missing from the plugin ") . '<strong>"' . $pluginParams['tag'] . '"</strong>';
 		$ret.= data_countdown_help();
-	    return $ret;
+		return $ret;
 	}
+	
 	$then = strtotime ($enddate);
-    if ($then == -1) { // strtotime failed so enddate was not a valid date
-	    $ret = tra("__Error__ - The plugin ") . '<strong>"' . $pluginParams['tag'] . '"</strong>' . tra(" was not given a valid date. The date given was:\n") . "enddate=$enddate";
-   	    return $ret;
-   	}
+	
+	if ($then == -1) { // strtotime failed so enddate was not a valid date
+		$ret = tra("__Error__ - The plugin ") . '<strong>"' . $pluginParams['tag'] . '"</strong>' . tra(" was not given a valid date. The date given was:\n") . "enddate=$enddate";
+		return $ret;
+	}
+	
 	if (isset($localtime) && $localtime == 'on') {
 		$tz = $_COOKIE['tz_offset'];
 	} else {
 		$tz = 0;
 	}
+	
+	if (!isset($class)) {
+		$class = 'countdown';
+	}
+	
+	if (!isset($punct)) {
+		$punct = " ";
+	}
+
+	if (!isset($text)) {
+		$text = "";
+	}
+
+	
 	$now = strtotime ("now") + $tz;
 	$difference = $then - $now;
 	$num = $difference/86400;
@@ -97,7 +131,15 @@ function data_countdown($data, $params) {
 	$mins = intval($num3);
 	$num4 = ($num3 - $mins)*60;
 	$secs = intval($num4);
-   	$ret = "$days ".tra("days").", $hours ".tra("hours").", $mins ".tra("minutes")." ".tra("and")." $secs ".tra("seconds")." $data";
+	$ret = "
+		<span class='".$class."'>"
+		. "<var>" . $days  . " " . tra("days")    . "</var>" . $punct
+		. "<var>" . $hours . " " . tra("hours")   . "</var>" . $punct
+		. "<var>" . $mins  . " " . tra("minutes") . "</var>" . $punct
+		. "<var>" . $secs  . " " . tra("seconds") . "</var>" . " "
+		. "<em>"  . $text  . "</em>"
+		. "</span>"
+	;
 	return $ret;
 }
 ?>
