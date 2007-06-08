749ebddb137b5335c14509d609b5e15752894704
Lester Caine <lester@lsces.co.uk>
2007-06-08 22:03:03 +0000

    Move PEAR.php inside liberty_inline_diff test. This needs more work and while I know people don't want to include these libraries with bitweaver HAVING to download PEAR is even worse!

--- content_history_inc.php
+++ content_history_inc.php
@@ -33,14 +33,14 @@ if (isset($_REQUEST["delete"]) && isset($_REQUEST["hist"])) {
 	/**
 	 * run 'pear install Text_Diff' to install the library,
 	 */ 
-	require_once('PEAR.php');
-	
-	if( $gBitSystem->isFeatureActive('liberty_inline_diff') && 
-		@include_once( 'Text/Diff.php' ) ) {	    
-		include_once( 'Text/Diff/Renderer/inline.php' );
-		$diff = &new Text_Diff($from_lines,$to_lines);
-		$renderer = &new Text_Diff_Renderer_inline();
-		$html = $renderer->render($diff);
+	if( $gBitSystem->isFeatureActive('liberty_inline_diff') ) {
+		require_once('PEAR.php');
+		if( @include_once( 'Text/Diff.php' ) ) {	    
+			include_once( 'Text/Diff/Renderer/inline.php' );
+			$diff = &new Text_Diff($from_lines,$to_lines);
+			$renderer = &new Text_Diff_Renderer_inline();
+			$html = $renderer->render($diff);
+		}
 	}
 	else {
 	    include_once( UTIL_PKG_PATH.'diff.php');
