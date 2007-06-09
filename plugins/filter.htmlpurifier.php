b22598f6e1ab1f99a037e5132c733b3ea9efe7fe
Max Kremmel <xing@synapse.plus.com>
2007-06-09 11:43:12 +0000

    rename functions to closer match other liberty plugin function names

--- plugins/filter.htmlpurifier.php
+++ plugins/filter.htmlpurifier.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Header: /cvsroot/bitweaver/_bit_liberty/plugins/filter.htmlpurifier.php,v 1.1 2007/06/09 11:20:41 squareing Exp $
+ * @version  $Header: /cvsroot/bitweaver/_bit_liberty/plugins/filter.htmlpurifier.php,v 1.2 2007/06/09 11:43:12 squareing Exp $
  * @package  liberty
  * @subpackage plugins_filter
  */
@@ -17,26 +17,26 @@ $pluginParams = array (
 	'help_page'            => 'Html Purifier',
 	'description'          => "Will try to sanitise any HTML output to make it HTML compliant.",
 	'auto_activate'        => FALSE,
-	'prefilter_function'   => 'htmlpure_prefilter',
-	'splitfilter_function' => 'htmlpure_splitfilter',
-	'postfilter_function'  => 'htmlpure_postfilter',
+	'prefilter_function'   => 'prefilter_htmlpure',
+	'splitfilter_function' => 'splitfilter_htmlpure',
+	'postfilter_function'  => 'postfilter_htmlpure',
 	'path'                 => LIBERTY_PKG_PATH.'plugins/filter.htmlpurifier.php',
 	'plugin_type'          => FILTER_PLUGIN,
 	'plugin_settings_url'  => LIBERTY_PKG_URL.'filter_htmlpurifier.php',
 );
 $gLibertySystem->registerPlugin( PLUGIN_GUID_FILTERHTMLPURIFIER, $pluginParams );
 
-function htmlpure_prefilter( $pFilterHash ) {
+function prefilter_htmlpure( $pFilterHash ) {
 	vd( 'prefilter' );
 	return $pFilterHash['data'];
 }
 
-function htmlpure_splitfilter( $pFilterHash ) {
+function splitfilter_htmlpure( $pFilterHash ) {
 	vd( 'splitfilter' );
 	return $pFilterHash['data'];
 }
 
-function htmlpure_postfilter( $pFilterHash ) {
+function postfilter_htmlpure( $pFilterHash ) {
 	vd( 'postfilter' );
 	return $pFilterHash['data'];
 }
