60d7a7c6d59729bf43944b697954c6a4d079f687
Max Kremmel <xing@synapse.plus.com>
2007-06-11 20:15:48 +0000

    do a permission check and return correct error depnding on what happened

--- ajax_comments.php
+++ ajax_comments.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_liberty/ajax_comments.php,v 1.1 2007/06/10 15:14:40 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_liberty/ajax_comments.php,v 1.2 2007/06/11 20:15:48 squareing Exp $
  * @package liberty
  * @subpackage functions
  */
 require_once( '../bit_setup_inc.php' );
-global $gContent, $gBitSmarty;
  
-require_once (LIBERTY_PKG_PATH.'LibertyContent.php');
-
 $staticContent = new LibertyContent();
-$gContent = $staticContent->getLibertyObject( $_REQUEST['parent_id'], $_REQUEST['parent_guid']);
+$gContent = $staticContent->getLibertyObject( $_REQUEST['parent_id'], $_REQUEST['parent_guid'] );
 
-if( $gContent->isCommentable() ) {
+if( !$gBitUser->hasPermission( 'p_liberty_post_comments' )) {
+	echo tra( "You do not have the required permissions to post new comments" );
+} elseif( $gContent->isCommentable() ) {
 	$commentsParentId = $_REQUEST['parent_id'];
 	$comments_return_url = $_REQUEST['comments_return_url'];
 	include_once( LIBERTY_PKG_PATH.'comments_inc.php' );
-	if (isset($_REQUEST['post_comment_submit'])){
+	if( isset( $_REQUEST['post_comment_submit'] )) {
 		$storeComment->loadComment();
 		$postComment = $storeComment->mInfo;
 		$postComment['parsed_data'] = $storeComment->parseData( $postComment );
@@ -24,7 +23,7 @@ if( $gContent->isCommentable() ) {
 	$gBitSmarty->assign('comment', $postComment);
 	$gBitSmarty->assign('commentsParentId', $commentsParentId);
 	echo $gBitSmarty->fetch( 'bitpackage:liberty/display_comment.tpl' );
-}else{
-	echo "Sorry, you do not have permission to post comments to this content.";
+} else {
+	echo tra( "Sorry, you can not post a comment here." );
 }
 ?>
