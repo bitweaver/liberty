d08876e949ec076a6e50873c7f23d223cab1d76d
Nick Palmer <nick@sluggardy.net>
2007-06-12 14:09:08 +0000

    Update to use bit_redirect.

--- redirect.php
+++ redirect.php
@@ -1,8 +1,7 @@
 <?php
 require_once('../bit_setup_inc.php');
 if (!empty($_REQUEST['q'])) {
-    // ToDo: It would be nice to be able to track hits out...
-    header( 'Location:'.$_REQUEST['q'] );
+    bit_redirect($_REQUEST['q'] );
 }
 else {
     $_REQUEST['error'] = tra('The redirect did not include a url.');
