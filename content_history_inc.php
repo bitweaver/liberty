66f1e24818cea0637aea5457fd54887a8efa3d50
Max Kremmel <xing@synapse.plus.com>
2007-07-11 19:47:54 +0000

    fall back to internal diff tools when pear isn't available but set

--- content_history_inc.php
+++ content_history_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Revision: 1.8 $
+ * @version  $Revision: 1.9 $
  * @package  liberty
  * @subpackage functions
  */
@@ -39,15 +39,12 @@ if (isset($_REQUEST["delete"]) && isset($_REQUEST["hist"])) {
 	/**
 	 * run 'pear install Text_Diff' to install the library,
 	 */ 
-	if( $gBitSystem->isFeatureActive('liberty_inline_diff') ) {
-		if( @include_once( 'PEAR.php') && @@include_once( 'Text/Diff.php' ) ) {	    
-			include_once( 'Text/Diff/Renderer/inline.php' );
-			$diff = &new Text_Diff($from_lines,$to_lines);
-			$renderer = &new Text_Diff_Renderer_inline();
-			$html = $renderer->render($diff);
-		}
-	}
-	else {
+	if( $gBitSystem->isFeatureActive( 'liberty_inline_diff' ) && @include_once( 'Text/Diff.php' )) {
+		include_once( 'Text/Diff/Renderer/inline.php' );
+		$diff = &new Text_Diff($from_lines,$to_lines);
+		$renderer = &new Text_Diff_Renderer_inline();
+		$html = $renderer->render($diff);
+	} else {
 	    include_once( UTIL_PKG_PATH.'diff.php');
 	    $diffx = new WikiDiff($from_lines,$to_lines);
 	    $fmt = new WikiUnifiedDiffFormatter;
