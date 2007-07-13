2cc7c80fc0924fae9e211a871d6aeddcec821e1c
Max Kremmel <xing@synapse.plus.com>
2007-07-13 22:49:46 +0000

    get mediawiki tables working when they are the first thing on the page

--- plugins/format.tikiwiki.php
+++ plugins/format.tikiwiki.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Revision: 1.106 $
+ * @version  $Revision: 1.107 $
  * @package  liberty
  */
 global $gLibertySystem;
@@ -523,7 +523,7 @@ class TikiWikiParser extends BitBase {
 	function parse_mediawiki_tables( $data ) {
 		//DEBUG: $data = "\n<!-- parse_mediawiki_tables() called. -->\n" . $data;
 		/* Find all matches to {|...|} with no {| inside. */
-		while (preg_match('/\n\{\|(.*?)\n\|\}/sm', $data, $matches)) {
+		while( preg_match( '/\n?\{\|(.*?)\n\|\}/sm', $data, $matches )) {
 			//DEBUG: vd($matches);
 			$table_data = str_replace("\r", "", $matches[1]);
 			$table_data = str_replace('||', "\n|", $table_data);
@@ -661,7 +661,7 @@ class TikiWikiParser extends BitBase {
 		$data = preg_replace("/\{rm\}/", "&rlm;", $data);
 
 		// Parse MediaWiki-style pipe syntax tables.
-		if ((strpos($data, "\n{|") !== FALSE) && (strpos($data, "\n|}") !== FALSE)) {
+		if(( strpos( $data, "{|" ) === 0 || strpos( $data, "\n{|" ) !== FALSE ) && strpos( $data, "\n|}" ) !== FALSE ) {
 			$data = $this->parse_mediawiki_tables($data);
 		}
 
