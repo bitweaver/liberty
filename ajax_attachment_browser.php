841e43503d9dd5d42fcc8b45de11ac5657e80309
Max Kremmel <xing@synapse.plus.com>
2007-09-26 06:10:23 +0000

    move around some whitespace and capitalise boolean values that code sticks to code guidelines

--- ajax_attachment_browser.php
+++ ajax_attachment_browser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_liberty/ajax_attachment_browser.php,v 1.7 2007/05/11 17:10:42 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_liberty/ajax_attachment_browser.php,v 1.8 2007/09/26 06:10:23 squareing Exp $
  * @package liberty
  * @subpackage functions
  */
@@ -12,16 +12,16 @@ require_once( '../bit_setup_inc.php' );
 global $gContent, $gBitSmarty;
 
 $gContent = new LibertyAttachable();
-if (isset($_REQUEST['content_id'])) {
+if( isset( $_REQUEST['content_id'] )) {
 	$gContent->mContentId = $_REQUEST['content_id'];
 }
-$gBitSmarty->assign('attachmentBrowser', true);
+$gBitSmarty->assign( 'attachmentBrowser', TRUE );
 include_once( LIBERTY_PKG_PATH.'attachment_browser.php' );
 
-if (isset($_REQUEST['json'])){
-	header('Content-type:application/json');
+if( isset( $_REQUEST['json'] )){
+	header( 'Content-type:application/json' );
 	$gBitSmarty->display( 'bitpackage:liberty/attachment_browser_json.tpl' );
-}else{
+} else {
 	echo $gBitSmarty->fetch( 'bitpackage:liberty/attachment_browser.tpl' );
 }
 ?>
