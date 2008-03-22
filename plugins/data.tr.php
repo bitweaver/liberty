5bc8434d35742c743150925c9ab0d25c834b985b
wjames5 <will@tekimaki.com>
2008-03-22 18:21:09 +0000

    wrap a string in a page for translation, good for small strings, not full pages

---
+++ plugins/data.tr.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * tr translation plugin
+ *
+ * @author     wjames5 will@tekimaki.com 
+ * @version    $Revision: 1.1 $
+ * @package    liberty
+ * @subpackage plugins_data
+ * @copyright  Copyright (c) 2008, bitweaver.org
+ */
+
+/**
+ * Setup Code
+ */
+define( 'PLUGIN_GUID_DATATR', 'datatr' );
+global $gLibertySystem;
+$pluginParams = array (
+	'tag'           => 'TR',
+	'auto_activate' => TRUE,
+	'requires_pair' => TRUE,
+	'load_function' => 'data_tr',
+	'title'         => 'Translate',
+	'help_page'     => 'DataPluginTR',
+	'description'   => tra( "Use this plugin to mark strings for translation. You should only use this for common short strings, and not entire pages." ),
+	'help_function' => 'data_tr_help',
+	'syntax'        => "{tr}",
+	'path'          => LIBERTY_PKG_PATH.'plugins/data.tr.php',
+	'security'      => 'registered',
+	'plugin_type'   => DATA_PLUGIN
+);
+$gLibertySystem->registerPlugin( PLUGIN_GUID_DATATR, $pluginParams );
+$gLibertySystem->registerDataTag( $pluginParams['tag'], PLUGIN_GUID_DATATR );
+
+function data_tr_help() {
+	$help = tra( "Example: " ) . "{tr}string of text to be translated{/tr}";
+	return $help;
+}
+
+function data_tr( $pData, $pParams, $pCommonObject ) {
+	$transString = tra( $pData );
+	$parseHash = $pCommonObject->mInfo;
+	$parseHash['no_cache'] = TRUE;
+	$parseHash['data'] = $transString;
+	return $pCommonObject->parseData( $parseHash );
+}
+?>
