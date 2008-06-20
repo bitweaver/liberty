ca6c96469e11a54cc1ff58b6d897323987dbfa6f
Lester Caine <lester@lsces.co.uk>
2008-06-20 07:45:19 +0000

    Need to remeber the real handler even if you are using default functions This seems wrong somehow - should only override plugin handler if it is invalid?

--- plugins/mime.application.php
+++ plugins/mime.application.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @version     $Header: /cvsroot/bitweaver/_bit_liberty/plugins/mime.application.php,v 1.1 2008/06/17 13:04:44 lsces Exp $
+ * @version     $Header: /cvsroot/bitweaver/_bit_liberty/plugins/mime.application.php,v 1.2 2008/06/20 07:45:19 lsces Exp $
  *
  * @author      xing  <xing@synapse.plus.com> 
- * @version     $Revision: 1.1 $
+ * @version     $Revision: 1.2 $
  * created      Thursday May 08, 2008
  * @package     liberty
  * @subpackage  liberty_mime_handler
@@ -26,14 +26,14 @@ global $gLibertySystem;
  * As a naming convention, the liberty mime handler definition should start with:
  * PLUGIN_MIME_GUID_
  */
-define( 'PLUGIN_MIME_GUID_APP', 'mimeapplication' );
+define( 'PLUGIN_MIME_GUID_APPLICATION', 'mimeapplication' );
 
 $pluginParams = array (
 	// Set of functions and what they are called in this paricular plugin
 	// Use the GUID as your namespace
 	'verify_function'    => 'mime_default_verify',
-	'store_function'     => 'mime_default_store',
-	'update_function'    => 'mime_default_update',
+	'store_function'     => 'mime_application_store',
+	'update_function'    => 'mime_application_update',
 	'load_function'      => 'mime_default_load',
 	'download_function'  => 'mime_default_download',
 	'expunge_function'   => 'mime_default_expunge',
@@ -54,8 +54,52 @@ $pluginParams = array (
 	// Here you can use a perl regular expression to pick out file extensions you want to handle
 	// e.g.: Some image types: '#^image/(jpe?g|gif|png)#i'
 	// This plugin will be picked if nothing matches.
-	//'mimetypes'          => array( '/.*/' ),
+	'mimetypes'           => array(
+		'#application/.*#i',
+	),
 );
-$gLibertySystem->registerPlugin( PLUGIN_MIME_GUID_APP, $pluginParams );
+$gLibertySystem->registerPlugin( PLUGIN_MIME_GUID_APPLICATION, $pluginParams );
+
+// depending on the scan the default file might not be included yet. we need to get it manually - simply use the relative path
+require_once( 'mime.default.php' );
+
+/**
+ * Store the data in the database
+ * 
+ * @param array $pStoreRow File data needed to store details in the database - sanitised and generated in the verify function
+ * @access public
+ * @return TRUE on success, FALSE on failure - $pStoreRow[errors] will contain reason
+ */
+function mime_application_store( &$pStoreRow ) {
+	// this will set the correct pluign guid, even if we let default handle the store process
+	$pStoreRow['attachment_plugin_guid'] = PLUGIN_MIME_GUID_APPLICATION;
+	$pStoreRow['log'] = array();
+
+	// if storing works, we process the image
+	if( $ret = mime_default_store( $pStoreRow )) {
+		// Dummy for later
+	}
+	return $ret;
+}
+
+/**
+ * mime_application_update update file information in the database if there were changes.
+ * 
+ * @param array $pStoreRow File data needed to update details in the database
+ * @access public
+ * @return TRUE on success, FALSE on failure - $pStoreRow[errors] will contain reason
+ */
+function mime_application_update( &$pStoreRow, $pParams = NULL ) {
+	$ret = FALSE;
+
+	// this will set the correct pluign guid, even if we let default handle the store process
+	$pStoreRow['attachment_plugin_guid'] = PLUGIN_MIME_GUID_APPLICATION;
+
+	// if storing works, we process the image
+	if( !empty( $pStoreRow['upload'] ) && $ret = mime_default_update( $pStoreRow )) {
+		// Dummy for later
+	}
+	return $ret;
+}
 
 ?>
