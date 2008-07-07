75454a0af7bba32fccbb176add0c988e140eba95
Max Kremmel <xing@synapse.plus.com>
2008-07-07 06:08:37 +0000

    setFormatHeader() has been moved to BitThemes

--- ajax_edit_storage.php
+++ ajax_edit_storage.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_liberty/ajax_edit_storage.php,v 1.5 2008/06/25 22:21:12 spiderr Exp $
- * @version  $Revision: 1.5 $
+ * $Header: /cvsroot/bitweaver/_bit_liberty/ajax_edit_storage.php,v 1.6 2008/07/07 06:08:37 squareing Exp $
+ * @version  $Revision: 1.6 $
  * @package  liberty
  * @subpackage functions
  */
@@ -19,6 +19,6 @@ include_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
 include_once( LIBERTY_PKG_PATH.'edit_storage_inc.php' );
 
 // fetch the content of the page to display
-$gBitSystem->setFormatHeader( 'center_only' );
+$gBitThemes->setFormatHeader( 'center_only' );
 $gBitSystem->display( 'bitpackage:liberty/edit_storage_list.tpl' , NULL, array( 'display_mode' => 'edit' ));
 ?>
