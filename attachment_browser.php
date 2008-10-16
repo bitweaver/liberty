6ca57aaffd9451516197de3f24a5802e36678ec9
Max Kremmel <xing@synapse.plus.com>
2008-10-16 09:00:25 +0000

    trailing whitespace

--- attachment_browser.php
+++ attachment_browser.php
@@ -3,7 +3,7 @@
  * attachment_browser
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.16 $
+ * @version  $Revision: 1.17 $
  * @package  liberty
  * @subpackage functions
  */
@@ -35,16 +35,16 @@ if (isset($_REQUEST['attachment_id']) && is_numeric($_REQUEST['attachment_id']))
 		'load_attached_to' => true,
 	);
 	$userAttachments = $gBitUser->getUserAttachments( $listHash );
-	
+
 	// Fake the storage assignment for edit_storage_list.tpl
 	$gContent->mStorage = $userAttachments;
 	$gBitSmarty->assign('gContent', $gContent);
-	
+
 	// pagination
 	$offset = @BitBase::verifyId( $_REQUEST['offset'] ) ? $_REQUEST['offset'] : 0;
 	$gBitSmarty->assign( 'curPage', $pgnPage = @BitBase::verifyId( $_REQUEST['pgnPage'] ) ? $_REQUEST['pgnPage'] : 1 );
 	$offset = ( $pgnPage - 1 ) * $gBitSystem->getConfig( 'max_records' );
-	
+
 	// calculate page number
 	$numPages = ceil( $listHash['cant'] / $gBitSystem->getConfig( 'max_records' ) );
 	$gBitSmarty->assign( 'cant', $listHash['cant'] );
