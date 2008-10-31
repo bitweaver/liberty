01734dd5bd7a8158854015705946fbb58183b46e
Max Kremmel <xing@synapse.plus.com>
2008-10-31 08:12:24 +0000

    renamed dep stuff to requirement stuff, clean up schema_inc files, move version info from info hash to registerPackageVersion90, move requirements to registerRequirements()

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -365,8 +365,8 @@ $gBitInstaller->registerUserPermissions( LIBERTY_PKG_NAME, array(
 	array('p_liberty_trusted_editor', 'Can make edits to content as a trusted editor', 'editors', LIBERTY_PKG_NAME ),
 ));
 
-// Package dependencies
-$gBitInstaller->registerDependencies( LIBERTY_PKG_NAME, array(
+// Package Requirements
+$gBitInstaller->registerRequirements( LIBERTY_PKG_NAME, array(
 	'users'     => array( 'min' => '2.1.0' ),
 	'kernel'    => array( 'min' => '2.0.0' ),
 	'themes'    => array( 'min' => '2.0.0' ),
