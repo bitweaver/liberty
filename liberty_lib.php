aec8c6382effac70c12cf0fa0bfb02b4eb04f0ed
spiderr <spider@viovio.com>
2011-10-04 02:43:43 -0400

    add clearstatcache inside liberty_generate_thumbnails

--- liberty_lib.php
+++ liberty_lib.php
@@ -739,6 +739,7 @@ function liberty_generate_thumbnails( &$pFileHash ) {
 			} else {
 				// create a subdirectory for the thumbs
 				$pFileHash['dest_branch'] = $initialDestPath.'thumbs/';
+				clearstatcache();
 				if( !is_dir( STORAGE_PKG_PATH.$pFileHash['dest_branch'] )) {
 					mkdir( STORAGE_PKG_PATH.$pFileHash['dest_branch'] );
 					clearstatcache();
