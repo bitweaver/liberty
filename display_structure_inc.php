b065614ffa87ef813999a952e4d0e048fd38836f
spiderr <spider@viovio.com>
2013-03-02 12:41:14 -0500

    setCanonicalLink for displaying with structure_id

--- display_structure_inc.php
+++ display_structure_inc.php
@@ -11,12 +11,13 @@
 /**
  * required setup
  */
-	global $gContent;
-	include_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
-	if( is_object( $gContent ) && $gContent->isValid() ) {
-		$gBitSystem->setBrowserTitle( $gStructure->getRootTitle().' : '.$gContent->getTitle() );
-		include $gContent->getRenderFile();
-	} else {
-		$gBitSystem->fatalError( tra( 'Page cannot be found' ), NULL, NULL, HttpStatusCodes::HTTP_NOT_FOUND );
-	}
-?>
+global $gContent;
+include_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
+if( is_object( $gContent ) && $gContent->isValid() ) {
+	$gBitSystem->setBrowserTitle( $gStructure->getRootTitle().' : '.$gContent->getTitle() );
+	$gBitSystem->setCanonicalLink( $gContent->getDisplayUri() );
+	include $gContent->getRenderFile();
+} else {
+	$gBitSystem->fatalError( tra( 'Page cannot be found' ), NULL, NULL, HttpStatusCodes::HTTP_NOT_FOUND );
+}
+
