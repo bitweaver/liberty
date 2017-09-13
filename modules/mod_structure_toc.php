5ddfdd6b348d2ce05104a112b805b2ddf630834d
spider@dev <spiderr@bitweaver.org>
2017-09-13 17:44:11 -0400

    clean up structures - auto ::load() in constructor; add root structure title to module; remove non w3c spec rel='up'

--- modules/mod_structure_toc.php
+++ modules/mod_structure_toc.php
@@ -19,20 +19,20 @@ $struct = NULL;
 if( is_object( $gStructure ) && $gStructure->isValid() && $gStructure->hasViewPermission() ) {
 	$struct = &$gStructure;
 } elseif( @BitBase::verifyId( $module_params['structure_id'] ) ) {
-		$struct = new LibertyStructure( $module_params['structure_id'] );
-		$struct->load();
+	$struct = new LibertyStructure( $module_params['structure_id'] );
 } elseif( is_object( $gContent ) && $gContent->hasViewPermission( FALSE ) ) {
 	$structures = $gContent->getStructures();
 	// We take the first structure. not good, but works for now - spiderr
 	if( !empty( $structures[0] ) ) {
 		require_once( LIBERTY_PKG_PATH.'LibertyStructure.php' );
 		$struct = new LibertyStructure( $structures[0]['structure_id'] );
-		$struct->load();
 	}
 }
 
 if( is_object( $struct ) && count( $struct->isValid() ) ) {
 	$_template->tpl_vars['moduleTitle'] = new Smarty_variable( $moduleParams['title'] );
 	$toc = $struct->getToc( $struct->mInfo['root_structure_id'], 'asc', FALSE, 2 );
+	$root = $struct->getRootObject( $struct->mInfo['root_structure_id'] );
+	$_template->tpl_vars['rootTitle'] = new Smarty_variable( $root->getDisplayLink() );
 	$_template->tpl_vars['modStructureTOC'] = new Smarty_variable( $struct->getToc( $struct->mInfo['root_structure_id'], 'asc', FALSE, 2 ) );
 }
--- templates/structure_toc_startul.tpl
+++ templates/structure_toc_startul.tpl
@@ -1,1 +1,1 @@
-<ol class="structure-toc" id="structure-branch-{$structureId}" structure_id="{$structureId}">
+<ol class="structure-toc branch-{$structureId}" structure_id="{$structureId}">
