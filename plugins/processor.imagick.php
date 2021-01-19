065335fb1e8013847623ae481f8dd3bd79ce7017
spiderr <spiderr@bitweaver.org>
2021-01-19 10:02:13 -0500

    add try/catch to imagick processor functions

--- plugins/processor.imagick.php
+++ plugins/processor.imagick.php
@@ -36,68 +36,73 @@ function liberty_imagick_resize_image( &$pFileHash ) {
 	global $gBitSystem;
 	$pFileHash['error'] = NULL;
 	$ret = NULL;
-	if( !empty( $pFileHash['source_file'] ) && is_file( $pFileHash['source_file'] )) {
-		$im = new Imagick();
-		$im->readImage( $pFileHash['source_file'] );
-		if( !$im->valid()) {
-			$destFile = liberty_process_generic( $pFileHash, FALSE );
-		} else {
-			$im->setCompressionQuality( $gBitSystem->getConfig( 'liberty_thumbnail_quality', 85 ));
-			$iwidth = $im->getImageWidth();
-			$iheight = $im->getImageHeight();
-			/*
-			 * the math on this was bad and the property assignments were bad - those are fixed. 
-			 * however this is disabled since its being invoked by default, which prevents the max height
-			 * from being enforced on portrait images which is counter intuitive. by default the bounding
-			 * rectangle should be enforced. if someone wants this feature, then a flag for this needs to
-			 * be created which will likely require some bigger change up stream, like in gThumbSizes and
-			 * in liberty_generate_thumbnails()
-			 * -wjames5
-			if((( $iwidth / $iheight ) < 1 ) && !empty( $pFileHash['max_width'] ) && !empty( $pFileHash['max_height'] )) {
-				// we have a portrait image, flip everything
-				$temp = $pFileHash['max_height'];
-				$pFileHash['max_height'] = $pFileHash['max_width'];
-				$pFileHash['max_width'] = $temp;
-			}
-			 */
-
-			// override $mimeExt if we have a custom setting for it
-			if( $gBitSystem->isFeatureActive( 'liberty_thumbnail_format' )) {
-				$mimeExt = $gBitSystem->getConfig( 'liberty_thumbnail_format' );
-			} else {
-				list( $type, $mimeExt ) = explode( '/', strtolower( $pFileHash['type'] ));
-			}
-
-			if( preg_match( "!(png|gif)!", $mimeExt )) {
-				$targetType = $mimeExt;
-				$destExt = '.'.$mimeExt;
-			} else {
-				$targetType = 'jpeg';
-				$destExt = '.jpg';
-			}
-
-			if( !empty( $pFileHash['max_width'] ) && !empty( $pFileHash['max_height'] ) && (( $pFileHash['max_width'] < $iwidth || $pFileHash['max_height'] < $iheight ) || $mimeExt != $targetType )) {
-				if( !empty( $pFileHash['dest_file'] ) ) {
-					$destFile = $pFileHash['dest_file'];
-				} else {
-					$destFile = STORAGE_PKG_PATH.$pFileHash['dest_branch'].$pFileHash['dest_base_name'].$destExt;
-				}
-				$pFileHash['name'] = $pFileHash['dest_base_name'].$destExt;
-
-				// create thumb and write
-				$im->thumbnailImage( $pFileHash['max_width'],  $pFileHash['max_height'], TRUE );
-				$im->writeImage( $destFile );
-
-				$pFileHash['size'] = filesize( $destFile );
-			} else {
+	if( !empty( $pFileHash['source_file'] ) && file_exists( $pFileHash['source_file'] ) && is_file( $pFileHash['source_file'] ) && (filesize( $pFileHash['source_file'] ) > 0) ) {
+		try { 
+			$im = new Imagick();
+			$im->readImage( $pFileHash['source_file'] );
+			if( !$im->valid()) {
 				$destFile = liberty_process_generic( $pFileHash, FALSE );
+			} else {
+				$im->setCompressionQuality( $gBitSystem->getConfig( 'liberty_thumbnail_quality', 85 ));
+				$iwidth = $im->getImageWidth();
+				$iheight = $im->getImageHeight();
+				/*
+				 * the math on this was bad and the property assignments were bad - those are fixed. 
+				 * however this is disabled since its being invoked by default, which prevents the max height
+				 * from being enforced on portrait images which is counter intuitive. by default the bounding
+				 * rectangle should be enforced. if someone wants this feature, then a flag for this needs to
+				 * be created which will likely require some bigger change up stream, like in gThumbSizes and
+				 * in liberty_generate_thumbnails()
+				 * -wjames5
+				if((( $iwidth / $iheight ) < 1 ) && !empty( $pFileHash['max_width'] ) && !empty( $pFileHash['max_height'] )) {
+					// we have a portrait image, flip everything
+					$temp = $pFileHash['max_height'];
+					$pFileHash['max_height'] = $pFileHash['max_width'];
+					$pFileHash['max_width'] = $temp;
+				}
+				 */
+
+				// override $mimeExt if we have a custom setting for it
+				if( $gBitSystem->isFeatureActive( 'liberty_thumbnail_format' )) {
+					$mimeExt = $gBitSystem->getConfig( 'liberty_thumbnail_format' );
+				} else {
+					list( $type, $mimeExt ) = explode( '/', strtolower( $pFileHash['type'] ));
+				}
+
+				if( preg_match( "!(png|gif)!", $mimeExt )) {
+					$targetType = $mimeExt;
+					$destExt = '.'.$mimeExt;
+				} else {
+					$targetType = 'jpeg';
+					$destExt = '.jpg';
+				}
+
+				if( !empty( $pFileHash['max_width'] ) && !empty( $pFileHash['max_height'] ) && (( $pFileHash['max_width'] < $iwidth || $pFileHash['max_height'] < $iheight ) || $mimeExt != $targetType )) {
+					if( !empty( $pFileHash['dest_file'] ) ) {
+						$destFile = $pFileHash['dest_file'];
+					} else {
+						$destFile = STORAGE_PKG_PATH.$pFileHash['dest_branch'].$pFileHash['dest_base_name'].$destExt;
+					}
+					$pFileHash['name'] = $pFileHash['dest_base_name'].$destExt;
+
+					// create thumb and write
+					$im->thumbnailImage( $pFileHash['max_width'],  $pFileHash['max_height'], TRUE );
+					$im->writeImage( $destFile );
+
+					$pFileHash['size'] = filesize( $destFile );
+				} else {
+					$destFile = liberty_process_generic( $pFileHash, FALSE );
+				}
 			}
+
+			// destroy object
+			$im->destroy();
+
+			$ret = $destFile;
+		} catch( ImagickException $e ) {
+			bit_error_log( $e->getMessage().' '.$pFileHash['source_file'] );
+			$ret = NULL;
 		}
-
-		// destroy object
-		$im->destroy();
-
-		$ret = $destFile;
 	} else {
 		$pFileHash['error'] = "No source file to resize";
 	}
@@ -108,15 +113,20 @@ function liberty_imagick_resize_image( &$pFileHash ) {
 function liberty_imagick_rotate_image( &$pFileHash ) {
 	$ret = FALSE;
 	if( !empty( $pFileHash['source_file'] ) && is_file( $pFileHash['source_file'] )) {
-		$im = new Imagick();
-		$im->readImage( $pFileHash['source_file'] );
-		if( !$im->valid()) {
-			$destFile = liberty_process_generic( $pFileHash, FALSE );
-		} elseif( empty( $pFileHash['degrees'] ) || !is_numeric( $pFileHash['degrees'] )) {
-			$pFileHash['error'] = tra( 'Invalid rotation amount' );
-		} else {
-			$im->rotateImage( new ImagickPixel(), $pFileHash['degrees'] );
-			$im->writeImage( $pFileHash['source_file'] );
+		try {
+			$im = new Imagick();
+			$im->readImage( $pFileHash['source_file'] );
+			if( !$im->valid()) {
+				$destFile = liberty_process_generic( $pFileHash, FALSE );
+			} elseif( empty( $pFileHash['degrees'] ) || !is_numeric( $pFileHash['degrees'] )) {
+				$pFileHash['error'] = tra( 'Invalid rotation amount' );
+			} else {
+				$im->rotateImage( new ImagickPixel(), $pFileHash['degrees'] );
+				$im->writeImage( $pFileHash['source_file'] );
+			}
+		} catch( ImagickException $e ) {
+			bit_error_log( $e->getMessage().' '.$pFileHash['source_file'] );
+			$pFileHash['error'] = $e->getMessage();
 		}
 	} else {
 		$pFileHash['error'] = "No source file to resize";
